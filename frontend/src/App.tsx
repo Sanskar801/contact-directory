d3ed7fff3e36ec2609c40aa491f17bca486602d0
Sanskar Jain <sanskarj257@gmail.com>
2025-10-27 16:38:17 +0530

    Search bar to screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import ContactList from "./components/ContactList";
 import ContactForm from "./components/ContactForm";
+import SearchContact from "./components/SearchContact";
 
 
 export default function App() {
   const [isContactFormOpen, setIsContactFormOpen] = useState(false);
   return (
     <div className="w-screen flex flex-col justify-between relative">
-      Hello
+      <SearchContact />
       <ContactList />
       <button
         className="bg-gray-300 w-fit"
