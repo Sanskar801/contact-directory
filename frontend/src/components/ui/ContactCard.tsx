dd25ed2c1555eb7afd2720d918d80bd0accecd4d
Sanskar Jain <sanskarj257@gmail.com>
2025-10-27 20:30:59 +0530

    Styles and some plain css

--- frontend/src/components/ui/ContactCard.tsx
+++ frontend/src/components/ui/ContactCard.tsx
@@ -6,16 +6,16 @@ export default function ContactCard() {
     return (
         <div className="text-white w-full h-fit cursor-pointer">
             <div
-                className="flex justify-between bg-slate-900 pl-4 p-3  rounded-xl"
+                className="flex justify-between bg-slate-900 pl-4 p-3  rounded"
                 onClick={() => setIsToggleOpen(!isToggleOpen)}
             >
                 <label className="">Ana Lee</label>
                 <div className="btn-grp flex gap-0.5 text-black">
-                    <button className="bg-gray-300 w-fit px-3 py-2 rounded cursor-pointer hover:scale-110 active:scale-95">E</button>
-                    <button className="bg-gray-300 w-fit px-3 py-2 rounded cursor-pointer hover:scale-110 active:scale-95">D</button>
+                    <button className="bg-gray-300 w-fit px-3 py-2 rounded cursor-pointer hover:scale-110 active:scale-95">Edit</button>
+                    <button className="bg-gray-300 w-fit px-3 py-2 rounded cursor-pointer hover:scale-110 active:scale-95">Delete</button>
                 </div>
             </div>
-            {isToggleOpen && <div className="flex flex-col bg-slate-700 p-2.5 pl-4 rounded-xl">
+            {isToggleOpen && <div className="flex flex-col bg-slate-700 p-2.5 pl-4 rounded rounded-b-2xl">
                 <label className="py-1">+91 9933448326</label>
                 <label className="py-1">analee@gmail.com</label>
             </div>}
