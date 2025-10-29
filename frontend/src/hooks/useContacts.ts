e9b2d92991f4d2b29525d5ee38572e084f5daea6
Sanskar Jain <sanskarj257@gmail.com>
2025-10-29 16:08:20 +0530

    Hooks for Contacts

---
+++ frontend/src/hooks/useContacts.ts
@@ -0,0 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+import type { PaginationParams, SearchParams } from "../types";
+import { queryKeys } from "../utils/queryClient";
+import { getAllContacts, searchContacts, getContactById } from '../services/contactServices'
+
+export function useContacts(params: PaginationParams) {
+    const sortParam = `${params.sort.field},${params.sort.direction}`;
+
+    return useQuery({
+        queryKey: queryKeys.contacts.list({
+            page: params.page,
+            size: params.size,
+            sort: sortParam,
+        }),
+        queryFn: () => getAllContacts(params),
+        staleTime: 5 * 60 * 1000,
+        placeholderData: (previousData) => previousData,
+    });
+}
+
+export function useSearchContacts(params: SearchParams) {
+    const sortParam = `${params.sort.field},${params.sort.direction}`;
+
+    return useQuery({
+        queryKey: queryKeys.search.query(params.query, {
+            page: params.page,
+            size: params.size,
+            sort: sortParam,
+        }),
+        queryFn: () => searchContacts(params),
+        enabled: params.query.length >= 2,
+        staleTime: 3 * 60 * 1000,
+    });
+}
+
+export function useContact(id: number) {
+    return useQuery({
+        queryKey: queryKeys.contact.detail(id),
+        queryFn: () => getContactById(id),
+        staleTime: 10 * 60 * 1000,
+    });
+}
